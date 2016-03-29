0f0a0b8affb56c5a66f9b159ab9e82b0b9d1e147
U-TACOS\Storm Gourley <storm.gourley@gmail.com>
2016-03-29 14:47:05 -0400

    Version 1.1 + Added user input for hero and error checks + Added commenting and structured code more + Changed graphing titles, and axis

--- b_simulator.py
+++ b_simulator.py
@@ -4,7 +4,8 @@ import random
 import numpy as np
 import matplotlib.pyplot as plt
 
-A = [2,40,40]
+# 1 Data initialization
+A = []
 B = [2,3,4]
 C = [50,45,40,35,30,25,20]
 D = [50,45,40,35,30,25,20]
@@ -12,10 +13,36 @@ A_count =[]
 B_count =[]
 results = []
 
+# 1.1 Error checking on user input
+while True:
+    try:
+        atk = int(input("Attack: "))
+        break # Breaks out of while loop
+    except ValueError:
+        print("Not an integer. Please try again.")
+        
+while True:      
+    try:
+        hp = int(input("Health: "))
+        break # Breaks out of while loop
+    except ValueError:
+        print("Not an integer. Please try again.")
+
+# 1.2 Creating the User's hero
+name = input("What is your name, champion?: ")
+A.append(atk)
+A.append(hp)
+A.append(A[1])
+print(name, "(",atk,"/",hp,")")
+
+# 2 Starts a battle simulation iterating over test attack values
 for n in range(0,len(B)):
+    # 2.1 For each attack, iterate over each possible health value
     for m in range(0,len(C)):
         temp = []
+        # 2.2 Define test size (neutral at 1000 runs per health)
         for i in range(1,1001):
+            # 2.3 Battle function ends when one health becomes < 1
             while A[1]>0 and C[m]>0:
                 A_roll = random.randint(1,12)
                 B_roll = random.randint(1,12)
@@ -43,7 +70,7 @@ n_groups = 21
 fig, ax = plt.subplots()
 
 index = np.arange(n_groups)
-bar_width = 0.2
+bar_width = 0.35
 
 opacity = 0.4
 
@@ -57,9 +84,9 @@ rects2 = plt.bar(index + bar_width, B_count, bar_width,
                  color='r',
                  label='B')
 
-plt.xlabel('# of Wins')
-plt.ylabel('Scores')
-plt.title('Scores by group and gender')
+plt.xlabel('Stat line')
+plt.ylabel('# of Wins')
+plt.title('# of Wins of %s V Stat line'%(name))
 plt.xticks(index + bar_width, ('Av2', 'Av2', 'Av2', 'Av2', 'Av2', 'Av2', 'Av2', 'Av3', 'Av3', 'Av3', 'Av3', 'Av3', 'Av3', 'Av3',
                                'Av4', 'Av4', 'Av4', 'Av4', 'Av4', 'Av4', 'Av4'))
 plt.legend()
