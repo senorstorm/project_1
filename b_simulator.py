8de1f7e4afc600a5b3ad00bf0b8a52878a578367
U-TACOS\Storm Gourley <storm.gourley@gmail.com>
2016-03-29 13:50:19 -0400

    Version 1.0 + Added new data sets to test again + Modified bar plot to show case A against all others + 21 total cases, each run 1000 times

--- b_simulator.py
+++ b_simulator.py
@@ -1,48 +1,67 @@
+%matplotlib inline
+
 import random
 import numpy as np
 import matplotlib.pyplot as plt
 
-A = [2,40]
-B = [3,30]
+A = [2,40,40]
+B = [2,3,4]
+C = [50,45,40,35,30,25,20]
+D = [50,45,40,35,30,25,20]
+A_count =[]
+B_count =[]
 results = []
 
-for i in range(1,1001):
-    while A[1]>0 and B[1]>0:
-        A_roll = random.randint(1,12)
-        B_roll = random.randint(1,12)
-        if A_roll>B_roll:
-            B[1] = B[1]-(A_roll-B_roll)*A[0]
-        else:
-            A[1] = A[1]-(B_roll-A_roll)*B[0]
-    
-    if A[1]<0:
-        results.append('B')
-    elif B[1]<0:
-        results.append('A')
-    A[1] = 40
-    B[1] = 30
+for n in range(0,len(B)):
+    for m in range(0,len(C)):
+        temp = []
+        for i in range(1,1001):
+            while A[1]>0 and C[m]>0:
+                A_roll = random.randint(1,12)
+                B_roll = random.randint(1,12)
+                if A_roll>B_roll:
+                    C[m] = C[m]-(A_roll-B_roll)*A[0]
+                else:
+                    A[1] = A[1]-(B_roll-A_roll)*B[n]
+            if A[1]<=0:
+                temp.append('B')
+            elif C[m]<=0:
+                temp.append('A')
+            A[1] = A[2]
+            C[m] = D[m]
+        results.append(temp)   
 
-A_count = results.count('A')
-B_count = results.count('B')
+for l in results:
+    A_count.append(l.count('A'))
+    B_count.append(l.count('B'))
 
+#print (results)
+print (A_count)
+print (B_count)
+
+n_groups = 21
 fig, ax = plt.subplots()
-index = np.arange(1)
-bar_width = 0.35
+
+index = np.arange(n_groups)
+bar_width = 0.2
+
+opacity = 0.4
 
 rects1 = plt.bar(index, A_count, bar_width,
-                 alpha=0.4,
+                 alpha=opacity,
                  color='b',
                  label='A')
 
 rects2 = plt.bar(index + bar_width, B_count, bar_width,
-                 alpha=0.4,
+                 alpha=opacity,
                  color='r',
                  label='B')
 
-plt.xlabel('Champion')
-plt.ylabel('Win #')
-plt.title('Win # by Champion')
-plt.xticks(index + bar_width, ('AvB'))
+plt.xlabel('# of Wins')
+plt.ylabel('Scores')
+plt.title('Scores by group and gender')
+plt.xticks(index + bar_width, ('Av2', 'Av2', 'Av2', 'Av2', 'Av2', 'Av2', 'Av2', 'Av3', 'Av3', 'Av3', 'Av3', 'Av3', 'Av3', 'Av3',
+                               'Av4', 'Av4', 'Av4', 'Av4', 'Av4', 'Av4', 'Av4'))
 plt.legend()
 
 plt.tight_layout()
